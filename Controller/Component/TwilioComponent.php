f3063e7e6a883b10d2e6e9c1bf5615514a5b78ea
Robert Annett <robert@manleys.co.uk>
2013-07-03 12:40:43 +0100

    Moved From to Config

--- Controller/Component/TwilioComponent.php
+++ Controller/Component/TwilioComponent.php
@@ -6,6 +6,8 @@ class TwilioComponent extends Component{
 	
 	protected $_twilio;
 	
+	protected $_from;
+	
 	function __construct()
 		{	
 			//load config
@@ -14,24 +16,47 @@ class TwilioComponent extends Component{
 			//get settings from config
 			$AccountSid = Configure::read('Twilio.AccountSid');
 			$AuthToken = Configure::read('Twilio.AuthToken');
+			$this->_from = Configure::read('Twilio.from');
 
 			//initialize the client
 			//$this->_twilio = new TwilioRestClient($this->account_sid, $this->auth_token);
 			$this->_twilio = new Services_Twilio($AccountSid, $AuthToken);
-
+			
 		}
 		
 	
-	public function sendSingleSmsMessage($from, $to, $message) {
+	public function sendSingleSms($to, $message) {
 		
 		$sms = $this->_twilio->account->sms_messages->create(
-		    $from, // From this number
+		    $this->_from, // From this number
 		    $to, // To this number
 		    $message
 		);
 		 
 		// Display a confirmation message on the screen
 		echo "Sent message {$sms->sid}";
+	}
+	
+	public function sendMultipleSms($to = array(), $message) {
+
+		$people = array(
+        	$to => "Name 1",
+        	$to => "Name 2",
+        	$to => "Name 3",
+    	);
+    	
+		foreach ($people as $number => $name) {
+	 
+	        $sms = $this->_twilio->account->sms_messages->create(
+	            $this->_from, 
+	            $number,
+	 
+	            // the sms body
+	            "Hi $name, this is a test!"
+	        );
+	 
+	        echo "Sent message to $name <br />";
+	    }
 				
 	}
 
--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Twilio-Plugin-CakePHP-2.0
 =========================
 
 Twilio Plugin for CakePHP 2.0+
+
+Include the Component in the controller you are using:
+public $components = array('Twilio.Twilio');
+
+In the Controller call the method:
+
+
